3d43c48714c869d194bbb19a1e4addd16eef6592
Jon Fairbanks <fairplay89@gmail.com>
2020-01-28 20:32:13 -0800

    Update var name

--- control.lua
+++ control.lua
@@ -1,16 +1,16 @@
 on_player_moved = function(event)
   local player = game.players[event.player_index]
-  local trains = game.surfaces[1].find_entities_filtered{type = "locomotive"}
-  for i, train in pairs(trains) do
+  local locomotive = game.surfaces[1].find_entities_filtered{type = "locomotive"}
+  for i, locomotive in pairs(trains) do
     -- Get chamfer distance - better than Manhattan but cheaper than Euclidean
-    local x = math.abs(train.position.x - player.position.x)
-    local y = math.abs(train.position.y - player.position.y)
+    local x = math.abs(locomotive.position.x - player.position.x)
+    local y = math.abs(locomotive.position.y - player.position.y)
     local distance = (math.max(x, y) * 5 + math.min(x, y) * (2)) / 5
     if distance < 5 then
-      --train.speed = 0
+      --locomotive.speed = 0
       game.print("Close to " .. train.name, {r = 0.5, g = 0, b = 0, a = 0.5})
     elseif distance > 5 then
-      --train.speed = 100
+      --locomotive.speed = 100
     end
   end
 end
